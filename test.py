dc14fb504c54749e1ac128d53d53695474b8c282
OLukyanenko91 <olukyanenko91@gmail.com>
2020-03-29 14:38:15 +0300

    Add XPATH using

--- test.py
+++ test.py
@@ -16,23 +16,24 @@ TIMEOUT = 10
 
 class ProfileTest(unittest.TestCase):
     def setUp(self):
+        self.path = os.path.abspath(os.getcwd())
         self.driver = webdriver.Chrome()
         self.driver.get(HOME_URL)
         time.sleep(DELAY)
 
     def testBase(self):
         # open 'career' page
-        self.driver.find_element_by_class_name("blog").click()
+        self.driver.find_element_by_xpath("//li[@class='blog']").click()
         WebDriverWait(self.driver, TIMEOUT).until(lambda d: d.current_url == CAREER_URL)
 
         # open 'hiring' page
-        self.driver.find_element_by_css_selector(".btn.green-btn").click()
+        self.driver.find_element_by_xpath("//a[@class='btn green-btn']").click()
         WebDriverWait(self.driver, TIMEOUT).until(lambda d: d.current_url == HIRING_URL)
 
         # upload incorrect file
         self.__uploadFile()
-        WebDriverWait(self.driver, TIMEOUT).until(lambda d: d.find_elements_by_css_selector(
-            "div[class='form-group has-error']"))
+        WebDriverWait(self.driver, TIMEOUT).until(lambda d: d.find_elements_by_xpath(
+             "//div[@class='form-group has-error']"))
 
         # fill personal data
         self.__fillPersonalData()
@@ -41,30 +42,29 @@ class ProfileTest(unittest.TestCase):
         self.driver.find_element_by_id("submit").click()
 
         # check warning color
-        warning = self.driver.find_element_by_class_name("warning-fields")
+        warning = self.driver.find_element_by_xpath("//p[@class='warning-fields help-block']")
         assert warning.value_of_css_property("color") == RED_COLOR
 
         # open home page
-        self.driver.find_element_by_class_name("logo-block").click()
+        self.driver.find_element_by_xpath("//div[@class='logo-block']").click()
         WebDriverWait(self.driver, TIMEOUT).until(lambda d: d.current_url == HOME_URL)
 
     def tearDown(self):
         self.driver.quit()
 
     def __uploadFile(self):
-        path = os.path.abspath(os.getcwd())
         self.driver.find_element_by_id("upload").click()
         time.sleep(DELAY)
-        subprocess.call(SUBPROCESS_CMD.format(path))
+        subprocess.call(SUBPROCESS_CMD.format(self.path))
 
     def __fillPersonalData(self):
         self.driver.find_element_by_id("inputName").send_keys("test")
         self.driver.find_element_by_id("inputLastname").send_keys("test")
         self.driver.find_element_by_id("inputEmail").send_keys("test")
         self.driver.find_element_by_id("inputPhone").send_keys("0000")
-        self.driver.find_element_by_name("bd").send_keys("1")
-        self.driver.find_element_by_name("bm").send_keys("1")
-        self.driver.find_element_by_name("by").send_keys("1")
+        self.driver.find_element_by_xpath("//select[@name='bd']").send_keys("1")
+        self.driver.find_element_by_xpath("//select[@name='bm']").send_keys("1")
+        self.driver.find_element_by_xpath("//select[@name='by']").send_keys("1")
 
 
 def main():
